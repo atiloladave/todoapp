83733fb971d4c14bf22bea3ecb211ed67462dc0f
atiloladave <atiloladavid1@gmail.com>
2021-01-03 20:19:36 +0100

    todo app

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="header">
+        <h1 id="v-step-0">TodoList</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
